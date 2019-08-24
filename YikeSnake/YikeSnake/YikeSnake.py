87d0301d5455d399dfe79dab7d0e17cec0e068e7
alagyn <benkimbrough44@gmail.com>
2019-08-24 19:36:43 -0400

    YikeSnake Mk1 implemented

--- YikeSnake/YikeSnake/YikeSnake.py
+++ YikeSnake/YikeSnake/YikeSnake.py
@@ -2,11 +2,13 @@
 import os
 
 import discord
+import consts
 
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+users = {}
 
 class Bot(discord.Client):
     
@@ -15,7 +17,21 @@ class Bot(discord.Client):
 
     async def on_message(self, message):
         if(message.author != self.user):
-            await message.channel.send(message.content)
-        
+            content = message.content.split(' ')
+            
+            if(len(content) == 2):
+                cmd = content[0]
+                if(cmd == consts.CMD):
+                    name = content[1]
+                
+                    if(name in users):
+                        users[name] += 1
+                    else:
+                        users.update({name : 1} )
+                
+                    await message.channel.send(name + '... yikes\nYou now have ' + str(users[name]) + ' yikes')
+            else:
+                await message.channel.send('Invalid Command')
+                
 bot = Bot()
 bot.run(token)
---
+++ YikeSnake/YikeSnake/consts.py
@@ -0,0 +1,2 @@
+# Constants
+CMD = '\\yike'
