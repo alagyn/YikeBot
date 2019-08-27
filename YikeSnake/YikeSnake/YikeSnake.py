fc41da3e8c898a65ab3cbad3643928b6a0ab5cd2
alagyn <benkimbrough44@gmail.com>
2019-08-26 21:06:51 -0400

    YikeSnake MK3.0, Stable: Memory Update
    Implemented the ability to save and load Yikes from
    a file.

--- YikeSnake/YikeSnake/YikeSnake.py
+++ YikeSnake/YikeSnake/YikeSnake.py
@@ -4,6 +4,7 @@ import os
 import discord
 import consts
 import re
+import sys
 
 from dotenv import load_dotenv
 
@@ -11,44 +12,62 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 users = {}
 
-class Bot(discord.Client):
-    
+class YikeSnake(discord.Client):
+
     async def on_ready(self):
         print(f'{self.user} is logged in')
         for g in self.guilds:
             for m in g.members:
                 users.update({str(m.id) : 0})
-
+        YikeSnake.readFile()
         game = discord.Game("_help")
         await self.change_presence(status=discord.Status.online, activity=game)
 
+    async def on_member_join(self, member):
+        for g in self.guilds:
+                for m in g.members:
+                    if(str(m.id) not in users):
+                        users.update({str(m.id) : 0})
+
+    def readFile():
+        with open(consts.LOG) as f:
+            for line in f:
+                data = line.split(":");
+                users.update({data[0] : int(data[1])})
+
+    def writeFile():
+        with open(consts.LOG, 'w') as f:
+            for id in users:
+                f.write(id + ":" + str(users[id]) + "\n")
+        
     async def on_message(self, message):
         if(message.author != self.user):
             content = message.content.split(' ')
             send = message.channel.send
-            channel = message.channel
             userFound = False
 
-            def sendUsage(send, cmd):
-                if(re.fullmatch(consts.YIKE_CMD, cmd):
-                    await send(consts.YIKE_USAGE)
-                elif(re.fullmatch(consts.UNYIKE_CMD):
-                    await send(consts.UNYIKE_USAGE)
+            #Funcs
+            async def sendUsage(cmd):
+                if(re.fullmatch(consts.YIKE_CMD, cmd)):
+                    await send("Usage:\n" + consts.YIKE_USAGE)
+                elif(re.fullmatch(consts.UNYIKE_CMD, cmd)):
+                    await send("Usage:\n" + consts.UNYIKE_USAGE)
                 
-
-            def updateYike(updateId, delta):
+            async def updateYike(updateId, delta):
                 users[updateId] += delta
                 if(users[updateId] < 0):
                     users[updateId] = 0
                     await send('NO NEGATIVE YIKES ALLOWED\nYou cheeky monkey')
                     return False
                 return True
-                
+            
             def getId(rawId):
                 if(rawId[2] == '!'):
                     return rawId[3:-1]
                 else:
                     return rawId[2:-1]
+
+            #####
 
             if(len(content) > 0 and len(content[0]) > 0 and content[0][0] == '_'):
                 
@@ -57,34 +76,35 @@ class Bot(discord.Client):
                 #Update Yikes
                 if(re.fullmatch(consts.YIKE_CMD, cmd) or re.fullmatch(consts.UNYIKE_CMD, cmd)):
                     
-                    done = False
+                    error = False
 
-                    if(len(content) != 2 or len(content) != 3 or not re.fullmatch(consts.ID_FORMAT, content[1])):
-                        sendUsage(send, cmd)
-                        done = True
+                    #Check command syntax
+                    if((len(content) != 2 and len(content) != 3) or not re.fullmatch(consts.ID_FORMAT, content[1])):
+                        await sendUsage(cmd)
+                        error = True
                     
-                    if(not done):
+                    if(not error):
                         deltaYike = 0
                         
                         if(re.fullmatch(consts.YIKE_CMD, cmd)):
                             deltaYike = 1
                         else:
                             deltaYike = -1
-    
-                        goof = False
-                        if(len(content) == 3):
-                            try:
-                                deltaYike *= int(content[2)
-                                
-                            except ValueError:
-                                sendUsage(send, cmd)
-                                goof = True
                         
-                        if(not goof):
-                            updateId = getId(content[1)
+                        #Check for optional amnt
+                        if(len(content) == 3):
+                            if(content[2].isnumeric()):
+                                amnt = int(content[2])
+                                deltaYike *= amnt
+                            else:
+                                await sendUsage(cmd)
+                                error = True
+                        
+                        if(not error):
+                            updateId = getId(content[1])
 
                             if(updateId in users):
-                                update = updateYike(updateId, deltaYike)
+                                update = await updateYike(updateId, deltaYike)
                                 
                                 if(update):
                                     if(deltaYike > 0):
@@ -98,12 +118,15 @@ class Bot(discord.Client):
                     
                 elif(re.fullmatch(consts.HELP_CMD, cmd)):
                     await send(consts.HELP_INFO)
-
+                
+                #List current yikes
                 elif(re.fullmatch(consts.LIST_CMD, cmd)):
+                    #List all
                     if(len(content) == 1):
                         list = 'CURRENT YIKE TOTALS:\n'
                         for m in message.guild.members:
                             name = ''
+                            #Check for Nickname
                             if(str(m.nick) != "None"):
                                 name = str(m.nick)
                             else:
@@ -111,18 +134,26 @@ class Bot(discord.Client):
 
                             list += "\t" + name + ": " + str(users[str(m.id)]) + "\n"
                         await send(list)
-                        
+                    #list single                                            
                     elif(len(content) == 2):
                         if(re.fullmatch(consts.ID_FORMAT, content[1])):
                             id = getId(content[1])
-                            await send('<@!' + id + '> has ' + str(users[id]) + ' yikes'
+                            await send('<@!' + id + '> has ' + str(users[id]) + ' yikes')
+                        else:
+                            await sendUsage(cmd)
                     else:
-                        sendUsage(send, cmd)
-                elif(re.fullmatch(consts.BIG_YIKE_CMD, cmd)):
-                    
+                        await sendUsage(cmd)
+                #Ultra Admin logout
+                elif(re.fullmatch(consts.LOGOUT_CMD, cmd)):
+                    if(str(message.author.id) == consts.ADMIN_ID):
+                        YikeSnake.writeFile()
+                        print('YikeLog Updated, Logging out')
+                        await self.change_presence(status=discord.Status.offline)
+                        await discord.Client.close(self)
+
                 #Invalid Command
                 else:
                     await send('Invalid Command: Try _help')
 
-bot = Bot()
+bot = YikeSnake()
 bot.run(token)
--- YikeSnake/YikeSnake/consts.py
+++ YikeSnake/YikeSnake/consts.py
@@ -5,22 +5,26 @@ YIKE_CMD = r"_(yike)"
 UNYIKE_CMD = r"_(unyike)"
 HELP_CMD = r"_(help)"
 LIST_CMD = r"_(list)"
+LOGOUT_CMD = r"_(logout)"
 
 ID_FORMAT = r"<@!?\d+>"
 
-#ToDo
-#List + user
-#Test yike + amnt
-#FullYike to show current totals
-
-
 #Data
+
+YIKE_USAGE = "_yike @user [optional amnt]\n" + \
+            "\tAdds one or more Yikes to a user\n"
+UNYIKE_USAGE = "_unyike @user [optional amnt]\n" + \
+                "\tRemoves one or more Yikes from a user\n"
+LIST_USAGE = "_list [optional @user]\n" + \
+            "\tLists the total Yikes for every user on a server or an individual\n"
+
+HELP_INFO = YIKE_USAGE + "\n" +  UNYIKE_USAGE + "\n" + LIST_USAGE + "\n" + \
+            "_help\n\tWhat do you think?"
+
+
+
+LOG = 'log.dat'
 YIKE_ID = "614235819498143744"
-HELP_INFO = "_yike @user\n" + \
-            "\tAdds one Yike to user\n" + \
-            "_unyike @user\n" + \
-            "\tRemoves one Yike from user\n" + \
-            "_list\n" + \
-            "\tLists the current Yike totals for this server\n" + \
-            "_help\n" + \
-            "\tWhat do you think?"
+ADMIN_ID = "324725726475714560"
+
+            
